7bcb4057007e97585027dc405ad67de1322bc020
Guido Espinoza <guidoespinozac@gmail.com>
2025-01-17 21:32:25 -0300

    avance dashboard NextJS

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
-import { Bebas_Neue } from "next/font/google"
+import { BsArrowRight } from "react-icons/bs"
+import Header from "./components/Header"
 import { bebas } from "./ui/fonts"
-import { FaReact } from "react-icons/fa"
 
 
 const Home = () => {
     return (
         <main className="flex min-h-screen flex-col">
-            <header className="bg-slate-800">
-                <div className="flex h-20 shrink-0 items-end md:h-52 p-10 w-4/5 mx-auto">
-                    <div className={`${bebas.className} flex flex-row items-center leading-none text-white`}>
-                        <FaReact className="h-20 w-20 rotate-[15deg]"/>
-                        <p className="text-[30px] ml-3">DonGudi-Dev</p>
-                    </div>
+            <Header />
+            <div className="mt-4 mx-auto flex grow flex-col gap-4 md:flex-row w-4/5">
+                <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+                    <p className={`${bebas.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+                        <strong>Welcome to DonGudi Dev</strong> Tutorial NextJS
+                    </p>
+                    <a href="#" className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
+                        <span>Login</span> <BsArrowRight />
+                    </a>
                 </div>
-            </header>
+                <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+                    <img src="/hero-desktop.png" alt="Screenshots of the dashboard" width={1000} height={700} className="hidden md:block" />
+                    <img src="/hero-mobile.png" alt="Screenshots of the dashboard" width={560} height={620} className="block md:hidden" />
+                </div>
+            </div>
         </main>
     )
 }
